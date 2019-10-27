4da00f27d72c60c17c0813311cb8c2a9534a56c3
yuhkiikd <yuhki.ikd@gmail.com>
2019-10-27 11:49:42 +0000

    011_詳細画面-空白の最寄駅情報が出ないよう処理

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,9 @@
 class Property < ApplicationRecord
+  validates :property, presence: true
+  validates :rent, presence: true
+  validates :address, presence: true
+  validates :age, presence: true
+  validates :note, presence: true
   has_many :station, dependent: :destroy
   accepts_nested_attributes_for :station
 end
